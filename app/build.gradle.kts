4ed79b0191a096e6fa85dbe5ded4ab371176ad60
aditya <aadityarock511@gmail.com>
2025-08-23 19:23:06 +0530

    added const

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.media3.exoplayer)
-    implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.foundation)
     implementation(libs.androidx.animation)
     implementation(libs.material3)
@@ -65,6 +63,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //exoplayer
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
 
     //For Image
     implementation(libs.coil.compose)
